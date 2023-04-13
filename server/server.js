5581f2e7b74cadeffede8afcef80b7c6ef0880eb
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-04-12 19:11:05 -0700

    fixed capitalization in user controller

--- server/server.js
+++ server/server.js
@@ -1,23 +1,4 @@
-// const app = require('./app')
-// const mongoose = require('mongoose')
 require('dotenv').config()
-
-// const MONGOURI = process.env.MONGO_URI
-// const PORT = process.env.PORT || 8000
-
-// mongoose.set('strictQuery', false)
-
-// const startServer = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI)
-//     console.log('Connected to database.')
-//   } catch (error) {
-//     console.log(error.message)
-//   }
-//   app.listen(PORT, () => console.log('Listening on ' + PORT + '...'))
-// }
-
-// startServer()
 
 const app = require('./app')
 const PORT = process.env.port || 8000
