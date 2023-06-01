04f759ec208752375304dbac7c8095c6b380251b
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-06-01 16:25:32 -0700

    Added static serve

--- server/app.js
+++ server/app.js
@@ -1,44 +1,45 @@
-const AppError = require('./utils/AppError')
-const errorHandler = require('./controllers/errorsController')
-const userRouter = require('./routes/userRouter')
-const placesRouter = require('./routes/placesRouter')
-const commentsRouter = require('./routes/commentsRouter')
-const repliesRouter = require('./routes/repliesRouter')
-const followersRouter = require('./routes/followersRouter')
-const alertsRouter = require('./routes/alertsRouter')
-const sseRouter = require('./routes/sseRouter')
-const path = require('path')
+const AppError = require('./utils/AppError');
+const errorHandler = require('./controllers/errorsController');
+const userRouter = require('./routes/userRouter');
+const placesRouter = require('./routes/placesRouter');
+const commentsRouter = require('./routes/commentsRouter');
+const repliesRouter = require('./routes/repliesRouter');
+const followersRouter = require('./routes/followersRouter');
+const alertsRouter = require('./routes/alertsRouter');
+const sseRouter = require('./routes/sseRouter');
+const path = require('path');
 
-const express = require('express')
-const { StatusCodes } = require('http-status-codes')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const { StatusCodes } = require('http-status-codes');
+const cors = require('cors');
+const app = express();
 
-const httpServer = require('http').createServer(app)
+const httpServer = require('http').createServer(app);
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // Production
-app.use('/', express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use('/', express.static(path.join(__dirname, '../client/build')))
 
-app.use('/api/v1/authentication', userRouter)
-app.use('/api/v1/places', placesRouter)
-app.use('/api/v1/comments', commentsRouter)
-app.use('/api/v1/reply', repliesRouter)
-app.use('/api/v1/follow', followersRouter)
-app.use('/api/v1/alerts', alertsRouter)
-app.use('/api/v1/sse', sseRouter)
+app.use('/api/v1/authentication', userRouter);
+app.use('/api/v1/places', placesRouter);
+app.use('/api/v1/comments', commentsRouter);
+app.use('/api/v1/reply', repliesRouter);
+app.use('/api/v1/follow', followersRouter);
+app.use('/api/v1/alerts', alertsRouter);
+app.use('/api/v1/sse', sseRouter);
 
 app.use('*', (req, res, next) => {
-  return next(new AppError(`Could not find ${req.originalUrl}`, StatusCodes.NOT_FOUND))
-})
+  return next(new AppError(`Could not find ${req.originalUrl}`, StatusCodes.NOT_FOUND));
+});
 
 // global error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
-const EventEmitter = require('events')
-const eventEmitter = new EventEmitter()
-app.set('eventEmitter', eventEmitter)
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+app.set('eventEmitter', eventEmitter);
 
-module.exports = httpServer
+module.exports = httpServer;
