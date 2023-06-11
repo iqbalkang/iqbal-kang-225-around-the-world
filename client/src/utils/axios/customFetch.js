ed68086124c7e420e0b4909a31c8593a6e264dc1
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-06-11 15:28:57 -0700

    Achanged axios link

--- client/src/utils/axios/customFetch.js
+++ client/src/utils/axios/customFetch.js
@@ -1,7 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const customFetch = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}/api/v1`,
-})
+  // baseURL: `${process.env.REACT_APP_SERVER_URL}/api/v1`,
+  baseURL: `http://54.215.113.53/api/v1`,
+});
 
-export default customFetch
+export default customFetch;
